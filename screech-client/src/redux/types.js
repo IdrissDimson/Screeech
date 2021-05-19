6c423c0729803866d7f97d5db2afc425b7034a62
Idriss Dimson <idriss.dimson@gmail.com>
2021-05-19 08:49:17 -0400

    added missing files

---
+++ screech-client/src/redux/types.js
@@ -0,0 +1,20 @@
+// User reducer types
+export const SET_AUTHENTICATED = 'SET_AUTHENTICATED';
+export const SET_UNAUTHENTICATED = 'SET_UNAUTHENTICATED';
+export const SET_USER = 'SET_USER';
+export const LOADING_USER = 'LOADING_USER';
+export const MARK_NOTIFICATIONS_READ = 'MARK_NOTIFICATIONS_READ';
+// UI reducer types
+export const SET_ERRORS = 'SET_ERRORS';
+export const LOADING_UI = 'LOADING_UI';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+export const LOADING_DATA = 'LOADING_DATA';
+export const STOP_LOADING_UI = 'STOP_LOADING_UI';
+// Data reducer types
+export const SET_SCREECHS = 'SET_SCREECHS';
+export const SET_SCREECH = 'SET_SCREECH';
+export const LIKE_SCREECH = 'LIKE_SCREECH';
+export const UNLIKE_SCREECH = 'UNLIKE_SCREECH';
+export const DELETE_SCREECH = 'DELETE_SCREECH';
+export const POST_SCREECH = 'POST_SCREECH';
+export const SUBMIT_COMMENT = 'SUBMIT_COMMENT';
