22a953b5876ea4de790d1b5d406507b7ca3eacb7
Idriss <IdrissDimson@users.noreply.github.com>
2021-09-21 11:57:44 -0400

    Update config.js

--- Screech-Functions/functions/util/config.js
+++ Screech-Functions/functions/util/config.js
@@ -1,9 +1,1 @@
-module.exports = {
-  apiKey: 'AIzaSyCSWcgl8a455xRscFJd4DMHpqm6hZwh5RI',
-  authDomain: 'screech-259f4.firebaseapp.com',
-  databaseURL: 'https://screech-259f4.firebaseio.com',
-  projectId: 'screech-259f4',
-  storageBucket: 'screech-259f4.appspot.com',
-  messagingSenderId: '817797188905',
-  appId: '1:817797188905:web:57d6771f37ec75f680bb38'
-};
+
